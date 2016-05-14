f605b6e97dc26ce5ab91f0918f89c946a929ef69
me <mn5930@yahoo.com>
2016-05-14 20:17:28 +0200

    Just clean up the code

--- Interpreter.ts
+++ Interpreter.ts
@@ -10,19 +10,8 @@
 * i.e. which elements in the `objects` field of WorldState, correspond
 * to the ones referred to in the sentence. 
 *
-* Moreover, it has to derive what the intended goal state is and
-* return it as a logical formula described in terms of literals, where
-* each literal represents a relation among objects that should
-* hold. For example, assuming a world state where "a" is a ball and
-* "b" is a table, the command "put the ball on the table" can be
-* interpreted as the literal ontop(a,b). More complex goals can be
-* written using conjunctions and disjunctions of these literals.
-*
 * In general, the module can take a list of possible parses and return
-* a list of possible interpretations, but the code to handle this has
-* already been written for you. The only part you need to implement is
-* the core interpretation function, namely `interpretCommand`, which produces a
-* single interpretation for a single command.
+* a list of possible interpretations.
 */
 module Interpreter {
 
@@ -30,7 +19,7 @@ module Interpreter {
     // exported functions, classes and interfaces/types
 
 /**
-Top-level function for the Interpreter. It calls `interpretCommand` for each possible parse of the command. No need to change this one.
+Top-level function for the Interpreter. It calls `interpretCommand` for each possible parse of the command. 
 * @param parses List of parses produced by the Parser.
 * @param currentState The current state of the world.
 * @returns Augments ParseResult with a list of interpretations. Each interpretation is represented by a list of Literals.
@@ -68,10 +57,7 @@ Top-level function for the Interpreter. It calls `interpretCommand` for each pos
     */
     export interface Literal {
 	/** Whether this literal asserts the relation should hold
-	 * (true polarity) or not (false polarity). For example, we
-	 * can specify that "a" should *not* be on top of "b" by the
-	 * literal {polarity: false, relation: "ontop", args:
-	 * ["a","b"]}.
+	 * (true polarity) or not (false polarity). 
 	 */
         polarity : boolean;
 	/** The name of the relation in question. */
@@ -84,7 +70,6 @@ Top-level function for the Interpreter. It calls `interpretCommand` for each pos
     export function stringify(result : InterpretationResult) : string {
         return result.interpretation.map((literals) => {
             return literals.map((lit) => stringifyLiteral(lit)).join(" & ");
-            // return literals.map(stringifyLiteral).join(" & ");
         }).join(" | ");
     }
 
@@ -95,24 +80,12 @@ Top-level function for the Interpreter. It calls `interpretCommand` for each pos
     //////////////////////////////////////////////////////////////////////
     // private functions
     /**
-     * The core interpretation function. The code here is just a
-     * template; you should rewrite this function entirely. In this
-     * template, the code produces a dummy interpretation which is not
-     * connected to `cmd`, but your version of the function should
-     * analyse cmd in order to figure out what interpretation to
-     * return.
-     * @param cmd The actual command. Note that it is *not* a string, but rather an object of type `Command` (as it has been parsed by the parser).
+     * @param cmd The actual command. 
      * @param state The current state of the world. Useful to look up objects in the world.
-     * @returns A list of list of Literal, representing a formula in disjunctive normal form (disjunction of conjunctions). See the dummy interpetation returned in the code for an example, which means ontop(a,floor) AND holding(b).
+     * @returns A list of list of Literal, representing a formula in disjunctive normal form (disjunction of conjunctions). 
      */
 function interpretCommand(cmd : Parser.Command, state : WorldState) : DNFFormula {
-        // This returns a dummy interpretation involving two random objects in the world
-        var tags : string[] = Array.prototype.concat.apply([], state.stacks);
-        var a: string = tags[Math.floor(Math.random() * tags.length)];
-        var b: string = tags[Math.floor(Math.random() * tags.length)];
-        
-        let command = cmd.command;
-        
+        let command = cmd.command; 
         let interpretation : DNFFormula = [];
         
         if (command == "take") {
@@ -149,6 +122,88 @@ function interpretCommand(cmd : Parser.Command, state : WorldState) : DNFFormula
         return interpretation;
     }
     
+    
+    // returns list of tags for objects satisfying 'entity' in world 'state'
+    function getPossibleEntitieTags(entity : any,state : WorldState) : string[] {
+        
+        // return value
+        var result : any[] = [];
+        let targetObject = entity.object;
+
+        if (targetObject.location == null) {
+            let matchingStateObjectTags = filterFeatures(targetObject,state);
+            result = matchingStateObjectTags;
+        } else {
+            let location = targetObject.location;
+            let relation = location.relation;
+            let relativeEntity = location.entity;
+            
+            targetObject = targetObject.object;
+            
+            let possibleTargetTags = filterFeatures(targetObject,state);
+            let possibleRelativeTags = getPossibleEntitieTags(relativeEntity,state);
+            
+            for (let i = 0; i < possibleTargetTags.length; i++) {
+                let thisPossibleTargetTag = possibleTargetTags[i];
+                for (let j = 0; j < possibleRelativeTags.length; j++) {
+                    let thisRelativeTag = possibleRelativeTags[j];
+                    let thisPossibleTarget = state.objects[thisPossibleTargetTag];
+                    let thisRelative = state.objects[thisRelativeTag];
+                    if (relation == "inside") {
+                        if (thisRelative.form == "box"
+                            && isInSameStack(thisPossibleTargetTag,thisRelativeTag,state)
+                            && stackIndexOf(thisPossibleTargetTag,state) == stackIndexOf(thisRelativeTag,state) + 1) 
+                        {
+                                if (!(thisPossibleTarget.size == "large" && thisRelative.size == "small")) 
+                                {
+                                    result.push(thisPossibleTargetTag);
+                                }
+                        }
+                    } else if (relation == "ontop") {
+                        if ((isInSameStack(thisPossibleTargetTag,thisRelativeTag,state)
+                                && stackIndexOf(thisPossibleTargetTag,state) == stackIndexOf(thisRelativeTag,state) + 1) 
+                            || (thisRelativeTag == "floor"&& stackIndexOf(thisPossibleTargetTag,state) == 0)) 
+                        {
+                                result.push(thisPossibleTargetTag);
+                        }
+                    } else if (relation == "above") {
+                        if ((isInSameStack(thisPossibleTargetTag,thisRelativeTag,state)
+                                && stackIndexOf(thisPossibleTargetTag,state) > stackIndexOf(thisRelativeTag,state)) 
+                            || (thisRelativeTag == "floor")) 
+                        {
+                                result.push(thisPossibleTargetTag);
+                        }
+                    } else if (relation == "under") {
+                        if (isInSameStack(thisPossibleTargetTag,thisRelativeTag,state)
+                            && stackIndexOf(thisPossibleTargetTag,state) < stackIndexOf(thisRelativeTag,state)) 
+                        {
+                                result.push(thisPossibleTargetTag);
+                        }
+                    } else if (relation == "beside") {
+                        if (Math.abs(stackIndex(thisPossibleTargetTag,state) - stackIndex(thisRelativeTag,state)) == 1) 
+                        {
+                            result.push(thisPossibleTargetTag);
+                        }
+                    } else if (relation == "leftof") {
+                        if (stackIndex(thisPossibleTargetTag,state) - stackIndex(thisRelativeTag,state) == -1) 
+                        {
+                            result.push(thisPossibleTargetTag);
+                        }
+                    } else if (relation == "rightof") {
+                        if (stackIndex(thisPossibleTargetTag,state) - stackIndex(thisRelativeTag,state) == 1) 
+                        {
+                            result.push(thisPossibleTargetTag);
+                        }
+                    }
+                }
+            }
+        }
+        
+        return result;
+    }
+   
+
+	 // Imply the physical laws
     function assertPhysicalLaws(combs : any, relation : any, state : any) {
         // return value
         let result : any = [];
@@ -156,7 +211,6 @@ function interpretCommand(cmd : Parser.Command, state : WorldState) : DNFFormula
         for (let i = 0; i < combs.length; i++) {
             let targetTag = combs[i][0];
             let relativeTag = combs[i][1];
-            
             let target = state.objects[targetTag];
             let relative = state.objects[relativeTag];
             
@@ -193,95 +247,12 @@ function interpretCommand(cmd : Parser.Command, state : WorldState) : DNFFormula
         
         return result;
     }
-    
-    // returns list of tags for objects satisfying 'entity' in world 'state'
-    function getPossibleEntitieTags(entity : any,state : WorldState) : string[] {
-        
-        // return value
-        var result : any[] = [];
-        
-        let targetObject = entity.object;
-        if (targetObject.location == null) {
-            let matchingStateObjectTags = filterFeatures(targetObject,state);
-            result = matchingStateObjectTags;
-        } else {
-            let location = targetObject.location;
-            let relation = location.relation;
-            let relativeEntity = location.entity;
-            
-            targetObject = targetObject.object;
-            
-            let possibleTargetTags = filterFeatures(targetObject,state);
-            
-            let possibleRelativeTags = getPossibleEntitieTags(relativeEntity,state);
-            
-            for (let i = 0; i < possibleTargetTags.length; i++) {
-                let thisPossibleTargetTag = possibleTargetTags[i];
-                for (let j = 0; j < possibleRelativeTags.length; j++) {
-                    let thisRelativeTag = possibleRelativeTags[j];
-                    let thisPossibleTarget = state.objects[thisPossibleTargetTag];
-                    let thisRelative = state.objects[thisRelativeTag];
-                    if (relation == "inside") {
-                        if (thisRelative.form == "box"
-                            && isInSameStack(thisPossibleTargetTag,thisRelativeTag,state)
-                            && stackIndexOf(thisPossibleTargetTag,state) == stackIndexOf(thisRelativeTag,state) + 1
-                            ) {
-                                if (!(thisPossibleTarget.size == "large" && thisRelative.size == "small")) {
-                                    result.push(thisPossibleTargetTag);
-                                }
-                        }
-                    } else if (relation == "ontop") {
-                        if (
-                            (
-                                isInSameStack(thisPossibleTargetTag,thisRelativeTag,state)
-                                && stackIndexOf(thisPossibleTargetTag,state) == stackIndexOf(thisRelativeTag,state) + 1
-                            ) || (
-                                thisRelativeTag == "floor"
-                                && stackIndexOf(thisPossibleTargetTag,state) == 0
-                            )
-                           ) {
-                                result.push(thisPossibleTargetTag);
-                        }
-                    } else if (relation == "above") {
-                        if (
-                            (
-                                isInSameStack(thisPossibleTargetTag,thisRelativeTag,state)
-                                && stackIndexOf(thisPossibleTargetTag,state) > stackIndexOf(thisRelativeTag,state)
-                            ) || (
-                                thisRelativeTag == "floor"
-                            )
-                           ) {
-                                result.push(thisPossibleTargetTag);
-                        }
-                    } else if (relation == "under") {
-                        if (isInSameStack(thisPossibleTargetTag,thisRelativeTag,state)
-                            && stackIndexOf(thisPossibleTargetTag,state) < stackIndexOf(thisRelativeTag,state)
-                            ) {
-                                result.push(thisPossibleTargetTag);
-                        }
-                    } else if (relation == "beside") {
-                        if (Math.abs(stackIndex(thisPossibleTargetTag,state) - stackIndex(thisRelativeTag,state)) == 1) {
-                            result.push(thisPossibleTargetTag);
-                        }
-                    } else if (relation == "leftof") {
-                        if (stackIndex(thisPossibleTargetTag,state) - stackIndex(thisRelativeTag,state) == -1) {
-                            result.push(thisPossibleTargetTag);
-                        }
-                    } else if (relation == "rightof") {
-                        if (stackIndex(thisPossibleTargetTag,state) - stackIndex(thisRelativeTag,state) == 1) {
-                            result.push(thisPossibleTargetTag);
-                        }
-                    }
-                }
-            }
-        }
-        
-        return result;
-    }
-    
+   
+ 
     // returns the index of 'thisPossibleTargetTag' in it's respective stack
     function stackIndexOf(thisPossibleTargetTag : string, state : WorldState) : number {
         let stacks : string[][] = state.stacks;
+
         for (let i = 0; i < stacks.length; i++) {
             let stackIndexOf = stacks[i].indexOf(thisPossibleTargetTag);
             if (stackIndexOf > -1) return stackIndexOf;
@@ -289,9 +260,11 @@ function interpretCommand(cmd : Parser.Command, state : WorldState) : DNFFormula
         return -1;
     }
     
+
     // returns the stack index of 'thisPossibleTargetTag'
     function stackIndex(thisPossibleTargetTag : string, state : WorldState) : number {
         let stacks : string[][] = state.stacks;
+
         for (let i = 0; i < stacks.length; i++) {
             let stackIndexOf = stacks[i].indexOf(thisPossibleTargetTag);
             if (stackIndexOf > -1) return i;
@@ -299,16 +272,20 @@ function interpretCommand(cmd : Parser.Command, state : WorldState) : DNFFormula
         return -1;
     }
     
+ 
     // returns true if 'thisPossibleTargetTag' and 'thisRelativeTag' are in the same stack
     function isInSameStack(thisPossibleTargetTag : string,
                            thisRelativeTag : string,
                            state : WorldState) : boolean {
+
         let stackIndexTarget : number = stackIndex(thisPossibleTargetTag,state);
         let stackIndexRelative : number = stackIndex(thisRelativeTag,state);
+        
         return ((stackIndexTarget > -1) && (stackIndexTarget == stackIndexRelative))
                || thisRelativeTag == "floor";
     }
     
+ 
     // returns list of objects in 'stateObjects' matching 'targetObject' w.r.t the following features
     let matchFeatures = ["size","color","form"];
     function filterFeatures(targetObject : any, state : WorldState) : string[] {
@@ -319,7 +296,6 @@ function interpretCommand(cmd : Parser.Command, state : WorldState) : DNFFormula
         
         // return value
         var result : any[] = [];
-        
         let stateObjects : any = state.objects;
         
         for (let objTag in stateObjects) {
@@ -338,6 +314,7 @@ function interpretCommand(cmd : Parser.Command, state : WorldState) : DNFFormula
         return result;
     }
     
+ 
     // return the cartesian product of l1, l2
     function cartesianProd(l1 : any, l2 : any) : string[][] {
         
